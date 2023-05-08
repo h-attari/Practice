3957143ef588bd22ce6674af02eb9fbe3549a487
H. Attari <40593925+h-attari@users.noreply.github.com>
2023-05-08 16:17:29 +0530

    Time: 50 ms (8.43%) | Memory: 16.4 MB (5.37%) - LeetSync

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,17 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        for bracket in s:
-            if bracket in ("(", "{", "["):
-                stack.append(bracket)
-                continue
-            if bracket in (")", "}", "]") and stack == []:
-                return False
-            if bracket == ")":
-                if stack[-1] != "(":
-                    return False
+        for x in s:
+            if x in ("(", "[", "{"):
+                stack.append(x)
             else:
-                if ord(bracket)-2 != ord(stack[-1]):
+                if not stack:
                     return False
-            stack.pop(-1)
-        return stack == [] 
+                if x == ")" and stack[-1] == "(":
+                    stack.pop(-1)
+                elif x == "]" and stack[-1] == "[":
+                    stack.pop(-1)
+                elif x == "}" and stack[-1] == "{":
+                    stack.pop(-1)
+                else:
+                    return False
+        return stack == []
