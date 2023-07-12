f07ba37c2f32081012612902d2a9060ead7452cf
H. Attari <40593925+h-attari@users.noreply.github.com>
2023-07-12 17:46:49 +0530

    Time: 244 ms (70.61%) | Memory: 17.8 MB (94.64%) - LeetSync

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -2,11 +2,10 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if nums == []:
             return -1
+        
         mid_index = (len(nums)) // 2
         if nums[mid_index] == target:
             return mid_index
-        if len(nums) == 1:
-            return -1
         if nums[mid_index] < target:
             target_index = self.search(nums[mid_index+1:], target)
             return target_index if target_index == -1 else mid_index + target_index + 1
