79e9c2e9b7ee4573304427e2b39c9d0d870d7877
H. Attari <40593925+h-attari@users.noreply.github.com>
2023-08-07 14:28:16 +0530

    Time: 47 ms (94.13%) | Memory: 16.7 MB (90.55%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,31 +1,29 @@
-def find_row(matrix: List[List[int]], target: int) -> int:
-    first, last = 0, len(matrix)-1
-    while first <= last:
-        mid = (first + last) // 2
-        if matrix[mid][0] <= target:
-            if matrix[mid][-1] >= target:
-                return mid
-            first = mid + 1
-            continue
-        last = mid - 1
-    return 0
-
-
-def find_num(lst: List[int], target: int) -> bool:
-    first, last = 0, len(lst)-1
-    while first <= last:
-        mid = (first + last) // 2
-        if lst[mid] == target:
-            return True
-        elif lst[mid] > target:
-            last = mid - 1
-        else:
-            first = mid + 1
-    return False
-
-
 class Solution:
+    def find_row(self, matrix: List[List[int]], target: int) -> int:
+        first, last = 0, len(matrix)-1
+        while first <= last:
+            mid = (first + last) // 2
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return mid
+            if target < matrix[mid][0]:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return -1
+    
+    def find_num(self, num_list: List[int], num: int) -> bool:
+        first, last = 0, len(num_list)-1
+        while first <= last:
+            mid = (first + last) // 2
+            if num_list[mid] == num:
+                return True
+            if num_list[mid] < num:
+                first = mid + 1
+            else:
+                last = mid - 1
+        return False
+    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row = find_row(matrix, target)
-        return find_num(matrix[row], target)
+        matrix_row = self.find_row(matrix, target)
+        return False if matrix_row == -1 else self.find_num(matrix[matrix_row], target)
         
