bf1a2aff505a176fe20755e6e6f43589d5c8007b
H. Attari <40593925+h-attari@users.noreply.github.com>
2023-08-14 14:17:55 +0530

    Time: 46 ms (99.30%) | Memory: 17.8 MB (42.76%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -3,5 +3,15 @@ class Solution:
         nums = set(nums)
         if len(nums) < 3:
             return max(nums)
-        nums = sorted(list(nums))
-        return nums[-3]
+        x, y, z = float("-inf"), float("-inf"), float("-inf")
+        for num in nums:
+            if num > x:
+                z = y
+                y = x
+                x = num
+            elif num > y:
+                z = y
+                y = num
+            elif num > z:
+                z = num
+        return z
