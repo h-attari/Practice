89b0108b74d056607a7ea948b745dd001c1a229c
H. Attari <40593925+h-attari@users.noreply.github.com>
2024-03-06 22:54:49 +0530

    Time: 50 ms (28.81%) | Memory: 19 MB (79.81%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,7 +1,7 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
-#         self.val = x
+#         self.val = xmklmlnsdjkcndskjvcbfdvrjknrjkva
 #         self.next = None
 
 class Solution:
