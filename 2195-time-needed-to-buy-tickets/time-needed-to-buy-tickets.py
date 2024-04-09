ce4eb914472498ea25fae7e143631d8bd47bf1f8
H. Attari <40593925+h-attari@users.noreply.github.com>
2024-04-09 22:45:02 +0530

    Time: 38 ms (85.49%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,12 +1,16 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        time = 0
-        while tickets[k] != 0:
-            for index in range(len(tickets)):
-                if tickets[index] > 0:
-                    tickets[index] -= 1
-                    time += 1
-                if index == k and tickets[index] == 0:
-                    break
+        if tickets[k] == 0:
+            return 0
+        
+        time = temp =tickets[k]
+        tickets[k] = 0
+        for index, ticket in enumerate(tickets):
+            if ticket < temp:
+                time += ticket
+            else:
+                time += temp
+                if index > k:
+                    time -= 1
         return time
         
