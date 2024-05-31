9aa530be6c5e763263151a69943ed532e1e8ab50
H. Attari <40593925+h-attari@users.noreply.github.com>
2024-05-31 23:30:42 +0530

    Time: 67 ms (20.05%) | Memory: 18.5 MB (76.83%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,10 +1,27 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        temp_set = set()
+        temp_num = 0
         for num in nums:
-            if num in temp_set:
-                temp_set.remove(num)
+            temp_num ^= num
+        
+        index = 0
+        while index < 32:
+            if (temp_num & (1 << index)) != 0:
+                break
+            index += 1
+        
+        lst1, lst2 = [], []
+        for num in nums:
+            if (num & (1 << index)) == 0:
+                lst1.append(num)
             else:
-                temp_set.add(num)
-        return list(temp_set)
+                lst2.append(num)
+        
+        a, b = 0, 0
+        for num in lst1:
+            a ^= num
+        for num in lst2:
+            b ^= num
+        
+        return [a, b]
         
