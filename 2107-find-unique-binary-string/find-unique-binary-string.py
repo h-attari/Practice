123a08ee33c1abf7ed756ab56fcaecf930d4be99
H. Attari <40593925+h-attari@users.noreply.github.com>
2025-02-20 11:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.88%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDifferentBinaryString(self, nums):
+        result = ""
+        for i in range(len(nums)):
+            if nums[i][i] == '0':
+                result += "1"
+            else:
+                result += "0"
+        return result
