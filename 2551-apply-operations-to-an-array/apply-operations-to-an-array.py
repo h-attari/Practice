a2b2412ee573eeb7175238269627fa2decb5a2a6
H. Attari <40593925+h-attari@users.noreply.github.com>
2025-03-01 07:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (74.68%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,9 +1,12 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+        temp = []
         for index in range(len(nums)-1):
             if nums[index] == nums[index+1]:
                 nums[index] *= 2
                 nums[index+1] = 0
-        temp = [num for num in nums if num > 0]
+            if nums[index] > 0:
+                temp.append(nums[index])
+        temp.append(nums[-1])
         return temp + ([0] * (len(nums) - len(temp)))
         
