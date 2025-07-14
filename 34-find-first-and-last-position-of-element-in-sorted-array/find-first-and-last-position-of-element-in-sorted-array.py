fc2dca106902f1aeb8a1491358c68401f0b572b5
H. Attari <40593925+h-attari@users.noreply.github.com>
2025-07-14 15:38:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (20.67%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,17 +1,35 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first, last = 0, len(nums)-1
-        while first <= last:
-            mid = (first + last) // 2
+    def search_first(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)-1
+        result = -1
+        while low <= high:
+            mid = low + ((high - low) // 2)
             if nums[mid] == target:
-                start, end = mid, mid
-                while start > 0 and nums[start-1] == target:
-                    start -= 1
-                while end < len(nums)-1 and nums[end+1] == target:
-                    end += 1
-                return [start, end]
-            if nums[mid] < target:
-                first = mid + 1
+                result = mid
+                high = mid - 1
+            elif nums[mid] > target:
+                high = mid - 1
             else:
-                last = mid - 1
-        return [-1, -1]
+                low = mid + 1
+        return result
+    
+    def search_last(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)-1
+        result = -1
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            if nums[mid] == target:
+                result = mid
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return result
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return [
+            self.search_first(nums, target),
+            self.search_last(nums, target)
+        ]
+        
