e928c6e33ccfc65b750d6e862527b1e6d7de2e2d
H. Attari <40593925+h-attari@users.noreply.github.com>
2025-10-26 00:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (89.38%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,14 +1,5 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        w = n // 7
-        money = w * 28
-        money += (7 * w *( w - 1))//2
-
-        if (n % 7):
-            extra_days = n % 7
-            money_to_add = w + 1
-            for i in range(extra_days):
-                money += money_to_add
-                money_to_add += 1
-
-        return money
+        q, r= divmod(n, 7)
+        return 28*q+7*q*(q-1)//2+(2*q+r+1)*r//2
+        
