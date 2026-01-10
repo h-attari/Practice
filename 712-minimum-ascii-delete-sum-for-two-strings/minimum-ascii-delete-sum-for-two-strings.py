72fb1bd66516e0df74796825c7a4959a4fe6f662
H. Attari <40593925+h-attari@users.noreply.github.com>
2026-01-10 12:38:59 +0530

    Time: 163 ms (99.40%) | Memory: 20.4 MB (72.97%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,18 +1,16 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        n=len(s1)
-        m=len(s2)
-        dp=[[0 for x in range(m+1)] for x in range(n+1)]
-        for i in range(1,n+1):
-            dp[i][0]=dp[i-1][0]+ord(s1[i-1])
+        n, m = len(s1), len(s2)
 
-        for i in range(1,m+1):
-            dp[0][i]=dp[0][i-1]+ord(s2[i-1])
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
+        # dp[i][j] = maximum ASCII sum of common subsequence
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(n):
+            for j in range(m):
+                if s1[i] == s2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + ord(s1[i])
                 else:
-                    dp[i][j]=min(dp[i-1][j]+ord(s1[i-1]),dp[i][j-1]+ord(s2[j-1]))
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
 
-        return dp[n][m]                
+        total_ascii = sum(ord(c) for c in s1) + sum(ord(c) for c in s2)
+        return total_ascii - 2 * dp[n][m]
