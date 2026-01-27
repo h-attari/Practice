6fde7c56c4294fa8f9edbed7cf1579557fb74082
H. Attari <40593925+h-attari@users.noreply.github.com>
2026-01-27 13:20:54 +0530

    Time: 698 ms (52.74%) | Memory: 68.6 MB (87.35%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -1,23 +1,22 @@
 class Solution:
     def minCost(self, n: int, edges: List[List[int]]) -> int:
-        adj=[[] for _ in range(n)]
-        for u, v, w in edges:
-            adj[u].append((w, v))
-            adj[v].append((w<<1, u))
+        graph = [[] for _ in range(n)]
 
-        dist=[0]+[inf]*(n-1)
-        pq=[(0, 0)]
-        heapq.heapify(pq)
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            graph[v].append((u, 2 * w))
+        
+        dist = [float("inf")] * n
+        dist[0] = 0
+        pq = [(0, 0)]
 
         while pq:
-            d, u=heapq.heappop(pq)
-
-            if d>dist[u]: continue
-            if u==n-1: return d
-
-            for w, v in adj[u]:
-                d2=d+w
-                if d2<dist[v]:
-                    dist[v]=d2
-                    heapq.heappush(pq, (d2, v))
-        return -1
+            cost, node = heapq.heappop(pq)
+            if cost > dist[node]:
+                continue
+            for nxt, w in graph[node]:
+                if dist[nxt] > cost + w:
+                    dist[nxt] = cost + w
+                    heapq.heappush(pq, (dist[nxt], nxt))
+        
+        return -1 if dist[n-1] == float("inf") else dist[n-1]
